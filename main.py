438edd7121e6aef5924041bb2be0c644eb00326c
Harrison <harrisonjwatters@gmail.com>
2023-03-02 17:47:12 -0500

    Added a rudimentary character creator

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 
+# Player class. Created when user completes character creation.
 class Player:
     def __init__(self, name, stats):
         self.name = name
@@ -7,8 +8,51 @@ class Player:
         self.items = []
         self.stats = stats
 
+
+
+# Broad encounter class. Properties TBD.
 class Encounter:
-    def __init__(self, id):
-        self.id = id
-        
+    def __init__(self):
+        pass
+
+
+
+# Combat Encounter class. Must include stats (bulk,swift,ego)
+class CombatEncounter(Encounter):
+    def __init__(self, stats):
+        # super.__init__()
+        self.stats = stats
+
+
+
+
+
+
+# Character Creation
+
+name = input("what is your name?")
+creation_points = 20
+
+print(f"you have {str(creation_points)} points left")
+
+bulk = int(input("how bulky are you?"))
+creation_points -= bulk
+
+print(f"you have {str(creation_points)} points left")
+swift = int(input("how nimble are you?"))
+creation_points -= swift
+
+print(f"you have {str(creation_points)} points left")
+ego = int(input("how popular are you?"))
+creation_points -= ego
+
+print(f"Bulk:{str(bulk)} Swift:{str(swift)} Ego:{str(ego)}")
+print(creation_points)
+
+player = Player(name=name, stats=(bulk,swift,ego))
+
+
+
+
+
         
