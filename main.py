fdd181eda3a30274c99549f86551858d27523ce5
Harrison <harrisonjwatters@gmail.com>
2023-03-02 17:04:23 -0500

    Small outline of some game objects

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+class Player:
+    def __init__(self, name, stats):
+        self.name = name
+        self.health = 100
+        self.items = []
+        self.stats = stats
+
+class Encounter:
+    def __init__(self, id):
+        self.id = id
+        
+        
